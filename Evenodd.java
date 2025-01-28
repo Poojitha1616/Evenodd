2da09a1292cfb32627bd24c063c6ae6d5ef8e21e
Poojitha1616 <gudemsaipoojitha2@gmail.com>
2025-01-28 22:36:48 +0530

    Add files via upload
    this program displays the number is even or odd

---
+++ Evenodd.java
@@ -0,0 +1,12 @@
+//•	Write a program to check if a number is even or odd using a conditional operator
+public class Evenodd {
+    public static void main(String[] args) {
+      int number = 5;
+      
+      if (number % 2 == 0) {
+        System.out.println(number + " is even.");
+      } else {
+        System.out.println(number + " is odd.");
+      }
+    }
+  }
